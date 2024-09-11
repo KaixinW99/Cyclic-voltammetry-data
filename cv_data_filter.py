e015116c7afdd9d2198913726b2353dd734cda67
KaixinW99 <kevinwang.sht@gmail.com>
2024-09-11 13:45:51 -0600

    compare the raw data with fiting one.

--- cv_data_filter.py
+++ cv_data_filter.py
@@ -11,6 +11,6 @@ data_name_without_extension = os.path.splitext(data_name)[0]
 b, a = SciSig.ellip(4, 0.01, 120, 0.125)
 E, I = np.genfromtxt(data_file, unpack=True, skip_header=1)
 y = SciSig.filtfilt(b, a, I, method="gust")
-#plt.plot(E, I, label='Original', linewidth=2)
+plt.plot(E, I, label='Original', linewidth=2)
 plt.plot(E, y, label='Filtered', linewidth=1)
 plt.show()
